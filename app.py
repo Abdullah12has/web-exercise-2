fb68c620b8dacbd67406692efd2899a5983f3df6
Syed Abdullah Hassan <56790074+Abdullah12has@users.noreply.github.com>
2025-02-19 01:06:36 +0200

    storage item added back

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ class Product(db.Model):
     handle = db.Column(db.String(80), unique=True, nullable=False)
     weight = db.Column(db.Float, nullable=False)
     price = db.Column(db.Float, nullable=False)
+    in_storage = db.relationship("StorageItem", back_populates="product", lazy=True)
+
+class StorageItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
+    location = db.Column(db.String(64), nullable=False)
+    qty = db.Column(db.Integer, nullable=False)
+    product = db.relationship("Product", back_populates="in_storage")
 
 class ProductCollection(Resource):
     def get(self):
